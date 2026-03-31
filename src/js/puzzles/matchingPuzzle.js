9959926b7e9639324e8d69d54c20a54ab8170f2a
AndyNachtsheim <andynachtsheim@gmail.com>
2026-03-30 23:21:50 -0500

    Add grid to start matching puzzle

--- src/js/puzzles/matchingPuzzle.js
+++ src/js/puzzles/matchingPuzzle.js
@@ -1,5 +1,40 @@
 export function startMatchingPuzzle({ containerID }) {
     containerID.innerHTML = `
-        <h3>Matching Puzzle</h3>
+        <header>
+            <h3>Matching Puzzle</h3>
+        </header>
+        <div class="grid-container">
+            <div class="grid-item">Tile 1</div>
+            <div class="grid-item">Tile 2</div>
+            <div class="grid-item">Tile 3</div>
+            <div class="grid-item">Tile 4</div>
+            <div class="grid-item">Tile 5</div>
+            <div class="grid-item">Tile 6</div>
+            <div class="grid-item">Tile 7</div>
+            <div class="grid-item">Tile 8</div>
+            <div class="grid-item">Tile 9</div>
+            <div class="grid-item">Tile 10</div>
+            <div class="grid-item">Tile 11</div>
+            <div class="grid-item">Tile 12</div>
+            <div class="grid-item">Tile 13</div>
+            <div class="grid-item">Tile 14</div>
+            <div class="grid-item">Tile 15</div>
+            <div class="grid-item">Tile 16</div>
+        </div>
     `;
+}
+
+
+const NUM_TILES = 16;
+const puzzleWindow = document.getElementById("puzzle-window");
+function setupMatchingPuzzle () {
+
+}
+
+function populateMatchingPuzzle () {
+
+}
+
+function checkForMatch() {
+
 }
