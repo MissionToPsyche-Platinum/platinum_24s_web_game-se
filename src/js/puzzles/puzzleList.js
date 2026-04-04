035e2a823f2cef28b923c362c2ebb295b092cd97
zschaadt <z.schaadt.2012@gmail.com>
2026-04-04 17:49:51 -0500

    Added puzzle help option with info for each puzzle.

--- src/js/puzzles/puzzleList.js
+++ src/js/puzzles/puzzleList.js
@@ -9,25 +9,31 @@ export const puzzles = [
     {
         name: "Tangram Puzzle",
         start: startTangramPuzzle,
+        helpText: "Arrange pieces to match the target shape",
     },
     {
         name: "Matching Puzzle",
         start: startMatchingPuzzle,
+        helpText: "Find and match pairs of similar items",
     },
     {
         name: "Number Logic Puzzle",
         start: startNumberLogicPuzzle,
+        helpText: "Use logic to solve the puzzle with numbers",
     },
     {
         name: "Maze Puzzle",
         start: startMazePuzzle,
+        helpText: "Navigate through the maze to reach the exit",
     },
     {
         name: "Sliding Puzzle",
         start: startSlidingPuzzle,
+        helpText: "Slide pieces to form the correct picture",
     },
     {
         name: "Tube Puzzle",
         start: startTubePuzzle,
+        helpText: "Turn the tubes to make the water flow!",
     }
 ];
