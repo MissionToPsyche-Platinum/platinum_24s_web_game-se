333334d70a98782c123c639f7a5e006c70845997
zschaadt <z.schaadt.2012@gmail.com>
2026-04-08 22:51:28 -0500

    Added dragging to tangram pieces

--- src/js/puzzles/tangramPuzzle.js
+++ src/js/puzzles/tangramPuzzle.js
@@ -8,13 +8,14 @@ export function startTangramPuzzle({ containerID }) {
     const svg = document.getElementById("tangram-svg");
 
     const pieces = [
-        { id: "piece1", points: "50,0 250,0 150,200", color: "#ff0000", inPlace: false },
-        { id: "piece2", points: "250,0 450,0 350,200", color: "#00ff2f", inPlace: false },
-        { id: "piece3", points: "0,400 100,200 200,400", color: "#ff7300", inPlace: false },
-        { id: "piece4", points: "200,400 300,200 400,400", color: "#ffff00", inPlace: false },
+        { id: "piece1", points: "50,0 250,0 150,200", color: "#ff0000", inPlace: false, x: 0, y: 0, rotation: 0 },
+        { id: "piece2", points: "250,0 450,0 350,200", color: "#00ff2f", inPlace: false, x: 0, y: 0, rotation: 0 },
+        { id: "piece3", points: "0,400 100,200 200,400", color: "#ff7300", inPlace: false, x: 0, y: 0, rotation: 0 },
+        { id: "piece4", points: "200,400 300,200 400,400", color: "#ffff00", inPlace: false, x: 0, y: 0, rotation: 0 },
     ];
 
     renderTangram(svg, pieces);
+    enableDragAndDrop(svg, pieces);
 }
 
 function renderTangram(svg, pieces) {
@@ -28,7 +29,46 @@ function renderTangram(svg, pieces) {
         polygon.setAttribute("id", piece.id);
         polygon.setAttribute("points", piece.points);
         polygon.setAttribute("fill", piece.color);
+        polygon.setAttribute("transform", `translate(${piece.x}, ${piece.y}) rotate(${piece.rotation})`);
+        polygon.style.cursor = "grab";
         svg.appendChild(polygon);
+    });
+}
+
+function enableDragAndDrop(svg, pieces) {
+    let selectedPiece = null;
+    let offsetX, offsetY;
+    svg.addEventListener("mousedown", (e) => {
+        const target = e.target;
+        if (target.tagName !== "polygon" && target.id === "outline") {
+            return;
+        }
+
+        const piece = pieces.find(p => p.id === target.id);
+        if (piece.inPlace) {
+            return;
+        }
+
+        selectedPiece = piece;
+        offsetX = e.clientX - piece.x;
+        offsetY = e.clientY - piece.y;
+        target.style.cursor = "grabbing";
+    });
+
+    svg.addEventListener("mousemove", (e) => {
+        if (!selectedPiece) return;
+
+        selectedPiece.x = e.clientX - offsetX;
+        selectedPiece.y = e.clientY - offsetY;
+        const target = document.getElementById(selectedPiece.id);
+        target.setAttribute("transform", `translate(${selectedPiece.x}, ${selectedPiece.y}) rotate(${selectedPiece.rotation})`);
+    });
+
+    svg.addEventListener("mouseup", (e) => {
+        if (!selectedPiece) return;
+        const target = document.getElementById(selectedPiece.id);
+        target.style.cursor = "grab";
+        selectedPiece = null;
     });
 }
 
