8a6076413b47a8cdedd539f8c677b01efa27685f
zschaadt <z.schaadt.2012@gmail.com>
2026-04-08 22:14:19 -0500

    Edited shapes to render fitting shapes

--- src/js/puzzles/tangramPuzzle.js
+++ src/js/puzzles/tangramPuzzle.js
@@ -1,15 +1,17 @@
 export function startTangramPuzzle({ containerID }) {
     containerID.innerHTML = `
         <div class="tangram-container">
-            <svg id="tangram-svg" width="800" height="400""></svg>
+            <svg id="tangram-svg" width="900" height="400""></svg>
         </div>
     `;
 
     const svg = document.getElementById("tangram-svg");
 
     const pieces = [
-        { id: "piece1", points: "600,0 800,0 700,100", color: "#ff0000", inPlace: false },
-        { id: "piece2", points: "400,0 600,0 500,200", color: "#00ff2f", inPlace: false },
+        { id: "piece1", points: "50,0 250,0 150,200", color: "#ff0000", inPlace: false },
+        { id: "piece2", points: "250,0 450,0 350,200", color: "#00ff2f", inPlace: false },
+        { id: "piece3", points: "0,400 100,200 200,400", color: "#ff7300", inPlace: false },
+        { id: "piece4", points: "200,400 300,200 400,400", color: "#ffff00", inPlace: false },
     ];
 
     renderTangram(svg, pieces);
@@ -18,7 +20,7 @@ export function startTangramPuzzle({ containerID }) {
 function renderTangram(svg, pieces) {
 
     svg.innerHTML = `
-        <polygon id="outline" points="0,0 400,0 400,400 0,400" fill="#120899" stroke="#000000" stroke-width="2"/>
+        <polygon id="outline" points="700,0 500,400 900,400" fill="#120899" stroke="#000000" stroke-width="2"/>
     `;
 
     pieces.forEach(piece => {
