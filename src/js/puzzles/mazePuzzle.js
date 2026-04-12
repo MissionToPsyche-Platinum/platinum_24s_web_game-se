a1083d273106f9015cb7f1ce4ffd1b0e540388dd
AndyNachtsheim <andynachtsheim@gmail.com>
2026-04-11 21:45:45 -0500

    Add basic maze frame to allow for additional functionality

--- src/js/puzzles/mazePuzzle.js
+++ src/js/puzzles/mazePuzzle.js
@@ -1,6 +1,20 @@
 export function startMazePuzzle({ containerID }) {
     containerID.innerHTML = `
-        <h3>Maze Puzzle</h3>
+        <div id="maze-puzzle-layout">
+            <header>
+                <h3 id="maze-puzzle-header">Maze Puzzle</h3>
+            </header>
+            <div class="maze-container" id="gridContainer">
+                <div id="maze-tile-0" class="maze-item">Start</div>
+                <div id="maze-tile-1" class="maze-item">Tile 1</div>
+                <div id="maze-tile-2" class="maze-item">Tile 2</div>
+                <div id="maze-tile-3" class="maze-item">Tile 3</div>
+                <div id="maze-tile-4" class="maze-item">Tile 4</div>
+                <div id="maze-tile-5" class="maze-item">Tile 5</div>
+                <div id="maze-tile-6" class="maze-item">Tile 6</div>
+                <div id="maze-tile-7" class="maze-item">End</div>
+            </div>
+        </div>
     `;
 }
 
@@ -11,4 +25,8 @@ class mazeTile {
         this.left = left;
         this.right = right;
     }
+}
+
+function populateMazePuzzle () {
+    
 }
