8b2ac8391e5117799630eb872e53ddc9c165cc62
Mia Laboranti <mialaboranti@Mias-MacBook-Pro.local>
2026-04-14 13:29:33 -0400

    Added more design elements to the settings screen, and changed the name selection portion into a placeholder

--- src/js/script.js
+++ src/js/script.js
@@ -323,20 +323,6 @@ function hideOverlay() {
   });
 }
 
-//Displays the overlay screens
-function showOverlay() {
-  overlays.forEach(overlay => {
-    overlay.style.display = "block";
-  });
-}
-
-//Hides the overlay screens
-function hideOverlay() {
-  overlays.forEach(overlay => {
-    overlay.style.display = "none";
-  });
-}
-
 //Displays a popup if the user did not enter a name
 function displayEmptyNameSelection() {
   nameCreationScreen.style.display = "none";
@@ -350,6 +336,9 @@ function openSettings() {
   closeExitConfirm();
   loadGameplaySettings();
   loadGameplaySettings();
+  if(!(firstName == null || lastName == null)) {
+    document.getElementById("settingDisplayName").textContent = firstName + " " + lastName;
+  }
   settingsPopUp.style.display = "block";
 }
 
