69d57d3f56a8d2e003b40c3cdc8523e6163368cb
AndyNachtsheim <andynachtsheim@gmail.com>
2026-04-14 16:57:57 -0500

    Add full arrow functionality

--- src/js/puzzles/mazePuzzle.js
+++ src/js/puzzles/mazePuzzle.js
@@ -6,14 +6,14 @@ export function startMazePuzzle({ containerID }) {
                 <h3 id="maze-puzzle-header">Maze Puzzle</h3>
             </header>
             <div class="maze-container" id="gridContainer">
-                <div id="maze-tile-0" class="maze-item">Start</div>
-                <div id="maze-tile-1" class="maze-item">Tile 1</div>
-                <div id="maze-tile-2" class="maze-item">Tile 2</div>
-                <div id="maze-tile-3" class="maze-item">Tile 3</div>
-                <div id="maze-tile-4" class="maze-item">Tile 4</div>
-                <div id="maze-tile-5" class="maze-item">Tile 5</div>
-                <div id="maze-tile-6" class="maze-item">Tile 6</div>
-                <div id="maze-tile-7" class="maze-item">End</div>
+                <div id="maze-tile-0" class="maze-item" tabIndex = "0">Start</div>
+                <div id="maze-tile-1" class="maze-item" tabIndex = "0">Tile 1</div>
+                <div id="maze-tile-2" class="maze-item" tabIndex = "0">Tile 2</div>
+                <div id="maze-tile-3" class="maze-item" tabIndex = "0">Tile 3</div>
+                <div id="maze-tile-4" class="maze-item" tabIndex = "0">Tile 4</div>
+                <div id="maze-tile-5" class="maze-item" tabIndex = "0">Tile 5</div>
+                <div id="maze-tile-6" class="maze-item" tabIndex = "0">Tile 6</div>
+                <div id="maze-tile-7" class="maze-item" tabIndex = "0">End</div>
             </div>
         </div>
     `;
@@ -22,19 +22,22 @@ export function startMazePuzzle({ containerID }) {
     populateMazePuzzle();
 
     mazeItems.forEach(item => {
-        //item.addEventListener('click', move);
-        item.addEventListener("keydown", (event) => {
+        item.addEventListener('keydown', (event) => {
             switch (event.key) {
                 case "ArrowUp":
+                    event.preventDefault();
                     moveUp();
                     break;
                 case "ArrowDown":
+                    event.preventDefault();
                     moveDown();
                     break;
                 case "ArrowLeft":
+                    event.preventDefault();
                     moveLeft();
                     break;
                 case "ArrowRight":
+                    event.preventDefault();
                     moveRight();
                     break;
             }
@@ -68,26 +71,55 @@ function populateMazePuzzle () {
     const mazeItems = document.querySelectorAll(".maze-item");
 
     mazeItems.forEach((item, i) => {
-        item.textContent = location[i].position;
-    });
-}
-
-function moveUp() {
-    // if () {
-
-    // }
-}
-
-function moveRight() {
-    location[index].text = 0;
-    index++;
-    location[index].text = 1;
-    const mazeItems = document.querySelectorAll(".maze-item");
-    mazeItems.forEach((item, i) => {
-        // item.text = location[i].text;
         item.textContent = location[i].text;
-        // if (item.position === NUM_TILES - 1 && item.text === 1) {
-        //     solvePuzzle();
-        // }
     });
+}
+
+function moveUp () {
+    if (index >= NUM_TILES / 2) {
+        location[index].text = 0;
+        index  = index - NUM_TILES / 2;
+        location[index].text = 1;
+        const mazeItems = document.querySelectorAll(".maze-item");
+        mazeItems.forEach((item, i) => {
+            item.textContent = location[i].text;
+        });
+    }
+}
+
+function moveDown () {
+    if (index <= NUM_TILES / 2) {
+        location[index].text = 0;
+        index  = index + NUM_TILES / 2;
+        location[index].text = 1;
+        const mazeItems = document.querySelectorAll(".maze-item");
+        mazeItems.forEach((item, i) => {
+            item.textContent = location[i].text;
+        });
+    }
+}
+
+function moveRight () {
+    if (index < NUM_TILES - 1) {
+        location[index].text = 0;
+        index++;
+        location[index].text = 1;
+        const mazeItems = document.querySelectorAll(".maze-item");
+        mazeItems.forEach((item, i) => {
+            item.textContent = location[i].text;
+        });
+    }
+}
+
+function moveLeft () {
+    if (index > 0) {
+        location[index].text = 0;
+        index--;
+        location[index].text = 1;
+    
+        const mazeItems = document.querySelectorAll(".maze-item");
+        mazeItems.forEach((item, i) => {
+            item.textContent = location[i].text;
+        });
+    }
 }
