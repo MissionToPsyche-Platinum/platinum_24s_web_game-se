1d6a329cd666d54bdaca5dc85eeb52d90faabd72
Mia Laboranti <mialaboranti@Mias-MacBook-Pro.local>
2026-04-17 10:50:47 -0400

    Added Number Logic Puzzle UI (3x3 Sudoku)

--- src/js/puzzles/numberLogicPuzzle.js
+++ src/js/puzzles/numberLogicPuzzle.js
@@ -1,5 +1,21 @@
+//3 x 3 sudoku puzzle (Can scale later, potentially based on difficulty set?)
 export function startNumberLogicPuzzle({ containerID }) {
     containerID.innerHTML = `
-        <h3>Number Logic Puzzle</h3>
+        <div class = "number-logic-container" id = "numberLogicContainer">
+            <header>
+                <h3 id = "number-logic-header">Number Logic Puzzle</h3>
+            </header>
+            <div class = "logic-grid" id = "logicGrid">
+                <input type = "number" min = "1" max = "3">
+                <input type = "number" min = "1" max = "3">
+                <input type = "number" min = "1" max = "3">
+                <input type = "number" min = "1" max = "3">
+                <input type = "number" min = "1" max = "3">
+                <input type = "number" min = "1" max = "3">
+                <input type = "number" min = "1" max = "3">
+                <input type = "number" min = "1" max = "3">
+                <input type = "number" min = "1" max = "3">
+            </div>
+        </div>
     `;
 }
