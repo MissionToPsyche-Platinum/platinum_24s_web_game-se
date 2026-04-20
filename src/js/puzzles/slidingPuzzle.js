f1ed19078f6284c57499572f61bd497c8e626927
zschaadt <z.schaadt.2012@gmail.com>
2026-04-19 22:18:52 -0500

    Added basic grid UI

--- src/js/puzzles/slidingPuzzle.js
+++ src/js/puzzles/slidingPuzzle.js
@@ -2,4 +2,44 @@ export function startSlidingPuzzle({ containerID }) {
     containerID.innerHTML = `
         <h3>Sliding Puzzle</h3>
     `;
+
+    const state = [
+        {value: 0, finalPos: 0},
+        {value: 1, finalPos: 1},
+        {value: 2, finalPos: 2},
+        {value: 3, finalPos: 3},
+        {value: 4, finalPos: 4},
+        {value: 5, finalPos: 5},
+        {value: 6, finalPos: 6},
+        {value: 7, finalPos: 7},
+        {value: null, finalPos: 8}
+    ]
+
+    renderSlidingPuzzle (containerID, state);
+
+
+    
+}
+
+function renderSlidingPuzzle (container, state) {
+    
+
+    const grid = document.createElement("div");
+    grid.className = "sliding-grid"
+
+    state.forEach(tile => {
+        const btn = document.createElement("button");
+        btn.className = "sliding-tile";
+
+        if(tile.value === null) {
+            btn.classList.add("empty");
+            btn.disabled = true;
+        } else {
+            btn.textContent = tile.value;
+        }
+
+        grid.appendChild(btn);
+    });
+
+    container.appendChild(grid);
 }
