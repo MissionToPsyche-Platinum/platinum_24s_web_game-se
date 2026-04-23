8b702a8b91ba53da8638b7477def2bd4feb3e48a
zschaadt <z.schaadt.2012@gmail.com>
2026-04-22 23:07:48 -0500

    Added tile movement and win condition

--- src/js/puzzles/slidingPuzzle.js
+++ src/js/puzzles/slidingPuzzle.js
@@ -1,3 +1,5 @@
+import { solvePuzzle } from "../gameController.js";
+
 export function startSlidingPuzzle({ containerID }) {
     containerID.innerHTML = `
     <div class="sliding-wrapper">
@@ -6,7 +8,7 @@ export function startSlidingPuzzle({ containerID }) {
     </div>
 `;
 
-    const state = [
+    let state = [
         {value: 0, finalPos: 0},
         {value: 1, finalPos: 1},
         {value: 2, finalPos: 2},
@@ -18,19 +20,72 @@ export function startSlidingPuzzle({ containerID }) {
         {value: null, finalPos: 8}
     ]
 
+    state = shufflePieces (state);
     renderSlidingPuzzle (containerID, state);
 
 
     
 }
 
+function shufflePieces (pieces) {
+    const arr = [...pieces];
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+function getEmptyTile(state){
+    return state.findIndex(tile => tile.value === null);
+}
+
+function checkSwap (index , emptyIndex) {
+    const posValue = index - emptyIndex;
+    if(posValue === 3 || posValue == -3) return true;
+
+    if(posValue === -1 || posValue === 1){
+        return Math.floor(index / 3) === Math.floor(emptyIndex / 3);
+    } 
+
+    return false;
+}
+
+function swapTiles (state , index , emptyIndex) {
+    [state[index] , state[emptyIndex]] = [state[emptyIndex] , state[index]];
+}
+
+function checkWin(state){
+    const puzzleSolved = state.every((tile, index) => {
+        return index === tile.finalPos;
+    });
+
+    if(puzzleSolved) {
+        solvePuzzle();
+    }
+}
+
+function handleClick (index, state, container){
+    const emptyIndex = getEmptyTile(state);
+    
+    if(checkSwap(index , emptyIndex)){
+        swapTiles(state, index , emptyIndex);
+        renderSlidingPuzzle(container , state);
+        checkWin(state);
+    } else {
+        return;
+    }
+}
+
 function renderSlidingPuzzle (container, state) {
     
 
     const grid = document.getElementById("sliding-grid");
+
+    grid.innerHTML = "";
     
 
-    state.forEach(tile => {
+    state.forEach((tile , index) => {
         const btn = document.createElement("button");
         btn.className = "sliding-tile";
 
@@ -39,6 +94,9 @@ function renderSlidingPuzzle (container, state) {
             btn.disabled = true;
         } else {
             btn.textContent = tile.value;
+            btn.addEventListener("click", () => {
+                handleClick(index, state, container);
+            })
         }
 
         grid.appendChild(btn);
